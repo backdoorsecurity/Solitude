81ac7eda6b1281b58e4289bc2b7d123d199d873e
backdoorsecurity <backdoorsecurity@proton.me>
2026-05-13 12:33:50 -0700

    updated install.sh

--- install.sh
+++ install.sh
@@ -14,7 +14,9 @@ fi
 cd /tmp
 git clone https://github.com/backdoorsecurity/Solitude.git && cd Solitude
 cp -r host-os/dot-files/.config/* $HOME/.config
+sudo cp -r host-os/dot-files/.config/* $HOME/.config
 cp host-os/dot-files/.z* $HOME/
+sudo cp host-os/dot-files/.zshrc /root
 
 # allow $USER passwordless commands
 echo " "$USER" ALL=(ALL) NOPASSWD: ALL" > tmp/usermods
@@ -29,3 +31,6 @@ sudo mv /tmp/override.conf /etc/systemd/system/getty@tty1.service.d && systemctl
 echo -e "[Unit]\nDescription=Apply system and network profiles\nAfter=sockets.target\n\n[Service]\nType=oneshot\nExecStart=/home/"$USER"/.config/sway/scripts/startup.sh\nRemainAfterExit=no\n\n[Install]\nWantedBy=sockets.target > /etc/systemd/system/startup.service
 echo -e "[Unit]\nDescription=Switch cpu turbo and energy bias based on ac/battery\nAfter=sockets.target\n\n[Service]\nType=simple\nExecStart=/"$USER"/.config/sway/scripts/pwr_perf.sh\nRemainAfterExit=yes\n\n[Install]\nWantedBy=sockets.target" > /etc/systemd/system/pwr_perf.service
 systemctl enable startup.service, pwr_perf.service
+
+chsh -s /usr/bin/zsh
+sudo chsh -s /usr/bin/zsh
